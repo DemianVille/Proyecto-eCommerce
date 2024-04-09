239618931a50965891ba120d0446870c7bd15954
Demian Brinville Piquinela <demian111101@gmail.com>
2024-04-09 20:37:31 -0300

    controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,9 @@
+const userController = {
+  index: async (req, res) => {},
+  show: async (req, res) => {},
+  create: async (req, res) => {},
+  update: async (req, res) => {},
+  destroy: async (req, res) => {},
+};
+
+module.exports = userController;
