84941147297949ef860d1f4a2e262adf3296dff8
Demian Brinville Piquinela <demian111101@gmail.com>
2024-04-09 19:02:14 -0300

    modelo de usuario

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+
+class User extends Model {
+  static initModel(sequelize) {
+    User.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        firstname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lastname: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "user",
+      }
+    );
+    return User;
+  }
+}
+
+module.exports = User;
