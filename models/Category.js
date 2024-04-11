e50f9e5a45633bf50fbc02fd02018dc3658a10fa
alhena1125 <alhena1125@gmail.com>
2024-04-11 19:52:06 -0300

    Category

---
+++ models/Category.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Category extends Model {
+  static initModel(sequelize) {
+    Category.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        modelName: "category",
+      }
+    );
+    return Category;
+  }
+}
+
+module.exports = Category;
