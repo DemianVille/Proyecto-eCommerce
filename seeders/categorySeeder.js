286e831d9e830aef797f1a5aa00bf6ad022e280e
Sofía Rovati <sofia.b.rovati.f@gmail.com>
2024-04-12 20:20:02 -0300

    categories seeder update

---
+++ seeders/categorySeeder.js
@@ -0,0 +1,10 @@
+const { faker } = require("@faker-js/faker");
+const { Category } = require("../models");
+
+async function categorySeeder() {
+  const categories = ["footwear", "leotards", "bottoms", "tights"];
+  await Category.bulkCreate(categories);
+  console.log("The seeder of categories has been runned!");
+}
+
+module.exports = categorySeeder;
--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const userSeeder = require("./userSeeder");
 const adminSeeder = require("./adminSeeder");
+const categorySeeder = require("./categorySeeder");
 
 userSeeder();
 adminSeeder();
+categorySeeder();
 
 console.log("The seeders has been runned!");
