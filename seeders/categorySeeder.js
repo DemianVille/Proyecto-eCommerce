c5a5b625b05cef7ec647712cadab191a941cfd06
Sofía Rovati <sofia.b.rovati.f@gmail.com>
2024-04-12 20:38:44 -0300

    Small seeder update

--- seeders/categorySeeder.js
+++ seeders/categorySeeder.js
@@ -1,4 +1,3 @@
-const { faker } = require("@faker-js/faker");
 const { Category } = require("../models");
 
 async function categorySeeder() {
--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const userSeeder = require("./userSeeder");
 const adminSeeder = require("./adminSeeder");
 const categorySeeder = require("./categorySeeder");
+const productSeeder = require("./productSeeder");
 
 userSeeder();
 adminSeeder();
 categorySeeder();
+productSeeder();
 
 console.log("The seeders has been runned!");
