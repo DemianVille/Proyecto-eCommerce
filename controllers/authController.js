54fd2498713c835f078a7fedc4cf8cab00395928
Demian Brinville Piquinela <demian111101@gmail.com>
2024-04-18 19:01:49 -0300

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+const { Admin } = require("../models");
 
 const authController = {
   getToken: async (req, res) => {
     try {
       const { email, password } = req.body;
 
-      const user = await User.findOne({ where: { email } });
+      const user = await Admin.findOne({ where: { email } });
 
-      if (user === null) return res.json({ message: "Invalid credentials" });
+      if (!user || user.password !== password)
+        return res.json({ message: process.env.SECRET_WORD });
 
-      // const token = jwt.sign({ sub: "user123" }, "UnStringMuySecreto");
+      const token = jwt.sign({ sub: user.id }, process.env.SECRET_WORD);
 
-      return req.json("OK");
+      return res.status(200).json({ token });
     } catch (err) {
       console.error(err);
       return res.json({ message: "Ups! Something went wrong." });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const { expressjwt: checkJwt } = require("express-jwt");
 const router = express.Router();
 const userRoutes = require("./userRoutes");
 const adminRoutes = require("./adminRoutes");
@@ -8,7 +10,11 @@ const productRoutes = require("./productRoutes");
 const authRoutes = require("./authRoutes");
 
 router.use("/users", userRoutes);
-router.use("/admins", adminRoutes);
+router.use(
+  "/admins",
+  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] }),
+  adminRoutes
+);
 router.use("/categories", categoryRoutes);
 router.use("/orders", orderRoutes);
 router.use("/products", productRoutes);
