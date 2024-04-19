3b33990a7030c80605a458298ba9ec780b5de147
Demian Brinville Piquinela <demian111101@gmail.com>
2024-04-19 18:26:36 -0300

    generador de token para admin y usuarios

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { Admin } = require("../models");
+const { Admin, User } = require("../models");
 
 const authController = {
   getToken: async (req, res) => {
@@ -8,13 +8,29 @@ const authController = {
       const { email, password } = req.body;
 
       const admin = await Admin.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
 
-      if (!admin || admin.password !== password)
-        return res.json({ message: process.env.SECRET_WORD });
+      if (!admin || admin.password !== password) {
+        if (!user || user.password !== password) {
+          return res.json({ message: process.env.SECRET_WORD });
+        }
+      }
 
-      const token = jwt.sign({ sub: admin.id }, process.env.SECRET_WORD);
-
-      return res.status(200).json({ token });
+      if (admin) {
+        const token = jwt.sign(
+          { sub: admin.id, role: "Admin" },
+          process.env.SECRET_WORD
+        );
+        console.log(req.auth);
+        return res.status(200).json({ token });
+      } else if (user) {
+        const token = jwt.sign(
+          { sub: user.id, role: "User" },
+          process.env.SECRET_WORD
+        );
+        console.log(req.auth);
+        return res.status(200).json({ token });
+      }
     } catch (err) {
       console.error(err);
       return res.json({ message: "Ups! Something went wrong." });
