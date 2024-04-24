6ed2d02634f049f2728cf9d696ace79f6764006e
Demian Brinville Piquinela <demian111101@gmail.com>
2024-04-24 18:42:44 -0300

    userRutes update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,27 +4,15 @@ const userController = require("../controllers/userController");
 const { expressjwt: checkJwt } = require("express-jwt");
 const isAdmin = require("../middlewares/isAdmin");
 
-router.get(
-  "/",
-  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] }),
-  isAdmin,
-  userController.index
-);
-router.get(
-  "/:id",
-  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] }),
-  userController.show
-);
 router.post("/", userController.store);
-router.delete(
-  "/:id",
-  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] }),
-  userController.destroy
+
+router.use(
+  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] })
 );
-router.patch(
-  "/:id",
-  checkJwt({ secret: process.env.SECRET_WORD, algorithms: ["HS256"] }),
-  userController.update
-);
+
+router.get("/", isAdmin, userController.index);
+router.get("/:id", userController.show);
+router.delete("/:id", userController.destroy);
+router.patch("/:id", userController.update);
 
 module.exports = router;
