f7c85b7d0be299e305ba4ca10825123836b39c39
Demian Brinville Piquinela <demian111101@gmail.com>
2024-06-04 20:51:52 -0300

    back-fix

--- seeders/categorySeeder.js
+++ seeders/categorySeeder.js
@@ -2,9 +2,9 @@ const { Category } = require("../models");
 
 async function categorySeeder() {
   const categories = [
-    { name: "footwear" },
-    { name: "dancewear" },
-    { name: "men" },
+    { name: "Footwear" },
+    { name: "Dancewear" },
+    { name: "Men" },
   ];
   await Category.bulkCreate(categories);
   console.log("The seeder of categories has been runned!");
--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 Miembros del grupo.
 Anthony Piñeyro, Demian Brinville, Sofia Rovati, Alhena Latorre, Franco Rodriguez
  */
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const port = process.env.APP_PORT;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
