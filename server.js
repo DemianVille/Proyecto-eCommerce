f50956b89e70caf7b72945695c97f45413e9ad88
DemianVille <demian111101@gmail.com>
2024-11-08 01:45:19 -0300

    xasd

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const port = process.env.PORT || process.env.APP_PORT;
 
 const corsOptions = {
   origin: ["https://copellia.vercel.app", "https://copellia-admin.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
-app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use("/api", routes);
